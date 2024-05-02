8f625b151564ad805b13d10b82c528baf2c2fb7b
cuisanzhang <cuisanzhang@163.com>
2024-05-02 15:21:20 +0800

    第一个本-掌上修仙

---
+++ zsxx.js
@@ -0,0 +1,250 @@
+//  掌上修仙
+/**
+ * new Env("掌上修仙")
+ * cron 每天1次
+ * 变量名:zsxxCookie
+ * 变量值:http://zsxx.tuesjf.cn/ 网址后的token的值（去掉token=）
+ * 完整格式 643545-f225-7542-7jk4-3434434324
+ * 多账号 & 分割
+ */
+
+
+const $ = new Env("掌上修仙");
+const ckName = "zsxxCookie";
+
+
+const axios = require('axios');
+
+var userList = [];
+let accountsSplitor = "&"; //多账号分隔符
+let varSplitor = '#'; //多变量分隔
+
+
+class UserInfo {
+    constructor(index, str) {
+        this.index = index;
+        this.token = str;
+        //this.ua = str.split(varSplitor)[1];
+        this.getHeaders = {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "User-Agent": "Mozilla/5.0 (Linux; Android 9; vivo X21A Build/PKQ1.180819.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/68.0.3440.91 Mobile Safari/537.36 uni-app Html5Plus/1.0 (Immersed/28.0)",
+            "Accept-Encoding": "gzip",
+            "Connection": "Keep-Alive",
+        }
+        this.postHeaders = {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "User-Agent": "Mozilla/5.0 (Linux; Android 9; vivo X21A Build/PKQ1.180819.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/68.0.3440.91 Mobile Safari/537.36 uni-app Html5Plus/1.0 (Immersed/28.0)",
+            "Accept-Encoding": "gzip",
+            "Connection": "Keep-Alive",
+        }
+    }
+
+
+}
+
+
+async function main(user) {
+    console.log(`账号${user.index}开始`);
+
+    let userinfo_res = await getUserInfo(user);
+
+    if (userinfo_res) {
+        await signNunShow(user);
+
+        await upGvip(user);
+
+        for (let i = 0; i < 5; i++) {
+            let videoOrder = await getVideoOrder(user);
+
+            if (!videoOrder) {
+                return;
+            }
+            let waitTime = Math.floor(Math.random() * 60) + 60
+            console.log(`看广告 - 随机等待${waitTime}s`);
+            await $.wait(waitTime * 1000)
+
+            await lookVideo(user);
+
+            waitTime = Math.floor(Math.random() * 8) + 2
+            console.log(`随机等待${waitTime}s`);
+            await $.wait(waitTime * 1000)
+        }
+
+    } else {
+        console.log(`获取用户信息错误`);
+    }
+
+    console.log(`账号${user.index}完成`);
+}
+
+async function getUserInfo(user) {
+    const user_info = await axios({
+        method: 'get',
+        url: `http://zsxx.tuesjf.cn/apis/v1/user_info?token=${user.token}`,
+        headers: user.getHeaders,
+    });
+
+    if (user_info) {
+        if (user_info.data.code == 1) {
+            console.log(`账号${user.index}:${user_info.data.data.info.nickname},当前灵石${user_info.data.data.info.integral}`)
+            return true;
+        } else {
+            console.log(`获取账号${user.index}信息错误:${JSON.stringify(user_info.data) }`);
+       
+            return false;
+        }
+    }
+
+}
+
+async function signNunShow(user) {
+    const res = await axios({
+        method: 'get',
+        url: `http://zsxx.tuesjf.cn/apis/v1/signNunShow?token=${user.token}`,
+        headers: user.getHeaders,
+    });
+
+    if (res) {
+        console.log(`账号${user.index}:${res.data.msg}`)
+        return true;
+    } else {
+        console.log(`账号${user.index}signNunShow错误:${res.data}`);
+        return false;
+    }
+}
+
+async function upGvip(user) {
+    const res = await axios({
+        method: 'post',
+        url: `http://zsxx.tuesjf.cn/apis/v1/upGvip`,
+        headers: user.getHeaders,
+        data: {
+            token: user.token,
+        },
+    });
+
+    if (res) {
+        console.log(`账号${user.index}:${res.data.msg}`)
+        return true;
+    } else {
+        console.log(`账号${user.index}upGvip错误:${res.data}`);
+        return false;
+    }
+}
+
+async function getVideoOrder(user) {
+    const res = await axios({
+        method: 'get',
+        url: `http://zsxx.tuesjf.cn/apis/v1/getVideoOrder?token=${user.token}`,
+        headers: user.getHeaders,
+    });
+
+    if (res) {
+        console.log(`账号${user.index}开始看视频:${res.data.msg}`)
+        return true;
+    } else {
+        console.log(`账号${user.index}开始看视频错误:${res.data}`);
+        return false;
+    }
+}
+
+async function lookVideo(user) {
+    const res = await axios({
+        method: 'post',
+        url: `http://zsxx.tuesjf.cn/apis/v1/lookVideo`,
+        headers: user.getHeaders,
+        data: {
+            token: user.token,
+        },
+    });
+
+    if (res.code && res.code == 1) {
+        console.log(`账号${user.index}看视频:${res.data.msg}`)
+        return true;
+    } else {
+        console.log(`账号${user.index}看视频错误:${res.data}`);
+        return false;
+    }
+}
+
+//主程序执行入口
+!(async () => {
+    try {
+        await checkEnv();
+
+        for (const userinfo of userList) {
+
+
+            try {
+
+                await main(userinfo);
+            } catch (e) {
+                console.log(e)
+            }
+        }
+    } catch (e) {
+        throw e;
+    }
+})()
+    .catch((e) => { console.logErr(e), $.msg($.name, `⛔️ script run error!`, e.message || e) })
+    .finally(async () => {
+        $.done({ ok: 1 });
+    });
+
+
+//********************************************************
+/**
+ * 变量检查与处理
+ * @returns
+ */
+async function checkEnv() {
+    let userCookie = process.env[ckName] || "";
+    //let userCount = 0;
+
+    //console.log("userCookie:" + userCookie);
+    if (userCookie) {
+        // console.log(userCookie);
+        let users = userCookie.split(accountsSplitor);
+        let i = 0;
+
+        for (const user in users) {
+            userList.push(new UserInfo(i + 1, users[i]));
+
+            //.log("users=" + JSON.stringify(users));
+           // console.log("users[i]=" + JSON.stringify(users[i]));
+            i++;
+        }
+        //userCount = $.userList.length;
+    } else {
+        console.log("未找到CK");
+        return;
+    }
+
+    console.log(`共找到${userList.length}个账号`), true; //true == !0
+    return
+}
+
+// 发送消息
+async function sendMsg(message) {
+    if (!message) return;
+    try {
+        if ($.isNode()) {
+            try {
+                var notify = require('./sendNotify');
+            } catch (e) {
+                var notify = require('./utils/sendNotify');
+            }
+            await notify.sendNotify($.name, message);
+        } else {
+            $.msg($.name, '', message);
+        }
+    } catch (e) {
+        console.log(`\n\n----- ${$.name} -----\n${message}`);
+    }
+}
+
+
+
+/** ---------------------------------固定不动区域----------------------------------------- */
+//From chavyleung's Env.js
+function Env(t, e) { class s { constructor(t) { this.env = t } send(t, e = "GET") { t = "string" == typeof t ? { url: t } : t; let s = this.get; return "POST" === e && (s = this.post), new Promise(((e, r) => { s.call(this, t, ((t, s, a) => { t ? r(t) : e(s) })) })) } get(t) { return this.send.call(this.env, t) } post(t) { return this.send.call(this.env, t, "POST") } } return new class { constructor(t, e) { this.name = t, this.http = new s(this), this.data = null, this.dataFile = "box.dat", this.logs = [], this.isMute = !1, this.isNeedRewrite = !1, this.logSeparator = "\n", this.encoding = "utf-8", this.startTime = (new Date).getTime(), Object.assign(this, e), this.log("", `🔔${this.name}, 开始!`) } getEnv() { return "undefined" != typeof $environment && $environment["surge-version"] ? "Surge" : "undefined" != typeof $environment && $environment["stash-version"] ? "Stash" : "undefined" != typeof module && module.exports ? "Node.js" : "undefined" != typeof $task ? "Quantumult X" : "undefined" != typeof $loon ? "Loon" : "undefined" != typeof $rocket ? "Shadowrocket" : void 0 } isNode() { return "Node.js" === this.getEnv() } isQuanX() { return "Quantumult X" === this.getEnv() } isSurge() { return "Surge" === this.getEnv() } isLoon() { return "Loon" === this.getEnv() } isShadowrocket() { return "Shadowrocket" === this.getEnv() } isStash() { return "Stash" === this.getEnv() } toObj(t, e = null) { try { return JSON.parse(t) } catch { return e } } toStr(t, e = null) { try { return JSON.stringify(t) } catch { return e } } getjson(t, e) { let s = e; if (this.getdata(t)) try { s = JSON.parse(this.getdata(t)) } catch { } return s } setjson(t, e) { try { return this.setdata(JSON.stringify(t), e) } catch { return !1 } } getScript(t) { return new Promise((e => { this.get({ url: t }, ((t, s, r) => e(r))) })) } runScript(t, e) { return new Promise((s => { let r = this.getdata("@chavy_boxjs_userCfgs.httpapi"); r = r ? r.replace(/\n/g, "").trim() : r; let a = this.getdata("@chavy_boxjs_userCfgs.httpapi_timeout"); a = a ? 1 * a : 20, a = e && e.timeout ? e.timeout : a; const [i, o] = r.split("@"), n = { url: `http://${o}/v1/scripting/evaluate`, body: { script_text: t, mock_type: "cron", timeout: a }, headers: { "X-Key": i, Accept: "*/*" }, timeout: a }; this.post(n, ((t, e, r) => s(r))) })).catch((t => this.logErr(t))) } loaddata() { if (!this.isNode()) return {}; { this.fs = this.fs ? this.fs : require("fs"), this.path = this.path ? this.path : require("path"); const t = this.path.resolve(this.dataFile), e = this.path.resolve(process.cwd(), this.dataFile), s = this.fs.existsSync(t), r = !s && this.fs.existsSync(e); if (!s && !r) return {}; { const r = s ? t : e; try { return JSON.parse(this.fs.readFileSync(r)) } catch (t) { return {} } } } } writedata() { if (this.isNode()) { this.fs = this.fs ? this.fs : require("fs"), this.path = this.path ? this.path : require("path"); const t = this.path.resolve(this.dataFile), e = this.path.resolve(process.cwd(), this.dataFile), s = this.fs.existsSync(t), r = !s && this.fs.existsSync(e), a = JSON.stringify(this.data); s ? this.fs.writeFileSync(t, a) : r ? this.fs.writeFileSync(e, a) : this.fs.writeFileSync(t, a) } } lodash_get(t, e, s = void 0) { const r = e.replace(/\[(\d+)\]/g, ".$1").split("."); let a = t; for (const t of r) if (a = Object(a)[t], void 0 === a) return s; return a } lodash_set(t, e, s) { return Object(t) !== t || (Array.isArray(e) || (e = e.toString().match(/[^.[\]]+/g) || []), e.slice(0, -1).reduce(((t, s, r) => Object(t[s]) === t[s] ? t[s] : t[s] = Math.abs(e[r + 1]) >> 0 == +e[r + 1] ? [] : {}), t)[e[e.length - 1]] = s), t } getdata(t) { let e = this.getval(t); if (/^@/.test(t)) { const [, s, r] = /^@(.*?)\.(.*?)$/.exec(t), a = s ? this.getval(s) : ""; if (a) try { const t = JSON.parse(a); e = t ? this.lodash_get(t, r, "") : e } catch (t) { e = "" } } return e } setdata(t, e) { let s = !1; if (/^@/.test(e)) { const [, r, a] = /^@(.*?)\.(.*?)$/.exec(e), i = this.getval(r), o = r ? "null" === i ? null : i || "{}" : "{}"; try { const e = JSON.parse(o); this.lodash_set(e, a, t), s = this.setval(JSON.stringify(e), r) } catch (e) { const i = {}; this.lodash_set(i, a, t), s = this.setval(JSON.stringify(i), r) } } else s = this.setval(t, e); return s } getval(t) { switch (this.getEnv()) { case "Surge": case "Loon": case "Stash": case "Shadowrocket": return $persistentStore.read(t); case "Quantumult X": return $prefs.valueForKey(t); case "Node.js": return this.data = this.loaddata(), this.data[t]; default: return this.data && this.data[t] || null } } setval(t, e) { switch (this.getEnv()) { case "Surge": case "Loon": case "Stash": case "Shadowrocket": return $persistentStore.write(t, e); case "Quantumult X": return $prefs.setValueForKey(t, e); case "Node.js": return this.data = this.loaddata(), this.data[e] = t, this.writedata(), !0; default: return this.data && this.data[e] || null } } initGotEnv(t) { this.got = this.got ? this.got : require("got"), this.cktough = this.cktough ? this.cktough : require("tough-cookie"), this.ckjar = this.ckjar ? this.ckjar : new this.cktough.CookieJar, t && (t.headers = t.headers ? t.headers : {}, void 0 === t.headers.Cookie && void 0 === t.cookieJar && (t.cookieJar = this.ckjar)) } get(t, e = (() => { })) { switch (t.headers && (delete t.headers["Content-Type"], delete t.headers["Content-Length"], delete t.headers["content-type"], delete t.headers["content-length"]), t.params && (t.url += "?" + this.queryStr(t.params)), void 0 === t.followRedirect || t.followRedirect || ((this.isSurge() || this.isLoon()) && (t["auto-redirect"] = !1), this.isQuanX() && (t.opts ? t.opts.redirection = !1 : t.opts = { redirection: !1 })), this.getEnv()) { case "Surge": case "Loon": case "Stash": case "Shadowrocket": default: this.isSurge() && this.isNeedRewrite && (t.headers = t.headers || {}, Object.assign(t.headers, { "X-Surge-Skip-Scripting": !1 })), $httpClient.get(t, ((t, s, r) => { !t && s && (s.body = r, s.statusCode = s.status ? s.status : s.statusCode, s.status = s.statusCode), e(t, s, r) })); break; case "Quantumult X": this.isNeedRewrite && (t.opts = t.opts || {}, Object.assign(t.opts, { hints: !1 })), $task.fetch(t).then((t => { const { statusCode: s, statusCode: r, headers: a, body: i, bodyBytes: o } = t; e(null, { status: s, statusCode: r, headers: a, body: i, bodyBytes: o }, i, o) }), (t => e(t && t.error || "UndefinedError"))); break; case "Node.js": let s = require("iconv-lite"); this.initGotEnv(t), this.got(t).on("redirect", ((t, e) => { try { if (t.headers["set-cookie"]) { const s = t.headers["set-cookie"].map(this.cktough.Cookie.parse).toString(); s && this.ckjar.setCookieSync(s, null), e.cookieJar = this.ckjar } } catch (t) { this.logErr(t) } })).then((t => { const { statusCode: r, statusCode: a, headers: i, rawBody: o } = t, n = s.decode(o, this.encoding); e(null, { status: r, statusCode: a, headers: i, rawBody: o, body: n }, n) }), (t => { const { message: r, response: a } = t; e(r, a, a && s.decode(a.rawBody, this.encoding)) })) } } post(t, e = (() => { })) { const s = t.method ? t.method.toLocaleLowerCase() : "post"; switch (t.body && t.headers && !t.headers["Content-Type"] && !t.headers["content-type"] && (t.headers["content-type"] = "application/x-www-form-urlencoded"), t.headers && (delete t.headers["Content-Length"], delete t.headers["content-length"]), void 0 === t.followRedirect || t.followRedirect || ((this.isSurge() || this.isLoon()) && (t["auto-redirect"] = !1), this.isQuanX() && (t.opts ? t.opts.redirection = !1 : t.opts = { redirection: !1 })), this.getEnv()) { case "Surge": case "Loon": case "Stash": case "Shadowrocket": default: this.isSurge() && this.isNeedRewrite && (t.headers = t.headers || {}, Object.assign(t.headers, { "X-Surge-Skip-Scripting": !1 })), $httpClient[s](t, ((t, s, r) => { !t && s && (s.body = r, s.statusCode = s.status ? s.status : s.statusCode, s.status = s.statusCode), e(t, s, r) })); break; case "Quantumult X": t.method = s, this.isNeedRewrite && (t.opts = t.opts || {}, Object.assign(t.opts, { hints: !1 })), $task.fetch(t).then((t => { const { statusCode: s, statusCode: r, headers: a, body: i, bodyBytes: o } = t; e(null, { status: s, statusCode: r, headers: a, body: i, bodyBytes: o }, i, o) }), (t => e(t && t.error || "UndefinedError"))); break; case "Node.js": let r = require("iconv-lite"); this.initGotEnv(t); const { url: a, ...i } = t; this.got[s](a, i).then((t => { const { statusCode: s, statusCode: a, headers: i, rawBody: o } = t, n = r.decode(o, this.encoding); e(null, { status: s, statusCode: a, headers: i, rawBody: o, body: n }, n) }), (t => { const { message: s, response: a } = t; e(s, a, a && r.decode(a.rawBody, this.encoding)) })) } } time(t, e = null) { const s = e ? new Date(e) : new Date; let r = { "M+": s.getMonth() + 1, "d+": s.getDate(), "H+": s.getHours(), "m+": s.getMinutes(), "s+": s.getSeconds(), "q+": Math.floor((s.getMonth() + 3) / 3), S: s.getMilliseconds() }; /(y+)/.test(t) && (t = t.replace(RegExp.$1, (s.getFullYear() + "").substr(4 - RegExp.$1.length))); for (let e in r) new RegExp("(" + e + ")").test(t) && (t = t.replace(RegExp.$1, 1 == RegExp.$1.length ? r[e] : ("00" + r[e]).substr(("" + r[e]).length))); return t } queryStr(t) { let e = ""; for (const s in t) { let r = t[s]; null != r && "" !== r && ("object" == typeof r && (r = JSON.stringify(r)), e += `${s}=${r}&`) } return e = e.substring(0, e.length - 1), e } msg(e = t, s = "", r = "", a) { const i = t => { switch (typeof t) { case void 0: return t; case "string": switch (this.getEnv()) { case "Surge": case "Stash": default: return { url: t }; case "Loon": case "Shadowrocket": return t; case "Quantumult X": return { "open-url": t }; case "Node.js": return }case "object": switch (this.getEnv()) { case "Surge": case "Stash": case "Shadowrocket": default: return { url: t.url || t.openUrl || t["open-url"] }; case "Loon": return { openUrl: t.openUrl || t.url || t["open-url"], mediaUrl: t.mediaUrl || t["media-url"] }; case "Quantumult X": return { "open-url": t["open-url"] || t.url || t.openUrl, "media-url": t["media-url"] || t.mediaUrl, "update-pasteboard": t["update-pasteboard"] || t.updatePasteboard }; case "Node.js": return }default: return } }; if (!this.isMute) switch (this.getEnv()) { case "Surge": case "Loon": case "Stash": case "Shadowrocket": default: $notification.post(e, s, r, i(a)); break; case "Quantumult X": $notify(e, s, r, i(a)); case "Node.js": }if (!this.isMuteLog) { let t = ["", "==============📣系统通知📣=============="]; t.push(e), s && t.push(s), r && t.push(r), console.log(t.join("\n")), this.logs = this.logs.concat(t) } } log(...t) { t.length > 0 && (this.logs = [...this.logs, ...t]), console.log(t.join(this.logSeparator)) } logErr(t, e) { switch (this.getEnv()) { case "Surge": case "Loon": case "Stash": case "Shadowrocket": case "Quantumult X": default: this.log("", `❗️${this.name}, 错误!`, t); break; case "Node.js": this.log("", `❗️${this.name}, 错误!`, t.stack) } } wait(t) { return new Promise((e => setTimeout(e, t))) } done(t = {}) { const e = ((new Date).getTime() - this.startTime) / 1e3; switch (this.log("", `🔔${this.name}, 结束! 🕛 ${e} 秒`), this.log(), this.getEnv()) { case "Surge": case "Loon": case "Stash": case "Shadowrocket": case "Quantumult X": default: $done(t); break; case "Node.js": process.exit(1) } } }(t, e) }
